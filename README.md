3c88e04177315907bd09c79814110365bae401a0
amr17-ca <amroali.ca@gmail.com>
2020-12-16 15:18:24 -0500

    code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # javascriptBook
+git add .
+git commit -m "code"
+git push -u origin main
+                
---
+++ ch1/sec1/first.js
@@ -0,0 +1,15 @@
+// node first.js
+
+let a = 6
+let b = 7
+console.log(a * b)
+
+
+echo "# javascriptBook" >> README.md
+git init
+git add README.md
+git add .
+git commit -m "up to ch2"
+git branch -M main
+git remote add origin https://github.com/amr17-ca/javascriptBook.git
+                
